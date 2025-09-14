675c4d8328ab89fd4c99526959ab904a79c88a9b
Araut08 <mayurraut1001@gmail.com>
2025-09-14 20:54:00 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import './i18n/i18n.js'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';  // ✅ add this
+import App from './App.jsx';
+import './index.css';
+import './i18n/i18n.js';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>   {/* ✅ wrap your app in Router */}
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
-)
+);
